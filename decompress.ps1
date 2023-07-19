731e06ed0e66ed2e05b19d7e6d78b000c9925018
Naldo DJ <marinaldo.jesus@gmail.com>
2023-07-19 12:03:48 -0300

    Create decompress.ps1

---
+++ decompress.ps1
@@ -0,0 +1,19 @@
+param (
+    [string]$filePath
+)
+
+Add-Type -AssemblyName System.IO.Compression
+
+$bytes = [System.IO.File]::ReadAllBytes($filePath)
+$compressedContent = $bytes[14..($bytes.Length - 1)]
+$stream = New-Object System.IO.MemoryStream
+$stream.Write($compressedContent, 0, $compressedContent.Length)
+$stream.Position = 0
+
+$decompressor = New-Object System.IO.Compression.DeflateStream($stream, [System.IO.Compression.CompressionMode]::Decompress)
+$reader = New-Object System.IO.StreamReader($decompressor, [System.Text.Encoding]::UTF8)
+
+$inflated = $reader.ReadToEnd()
+$reader.Close()
+
+Write-Output ($inflated.Trim())
